7907d3f63cd8e151d6b4c3b835c713a0d141d52a
Laerte Allan de Oliveira Araujo <Laerte.araujo@fs.com.br>
2019-04-10 18:47:33 -0300

    Fixed bug
    Fixed Bug

--- manager.py
+++ manager.py
@@ -3,13 +3,11 @@ import sys
 from challenge import views
 from challenge.config import get_config
 from challenge.server import Server
-from challenge.service.singleton_zap_group import SingletonGroupZap
 
 config_challenge = get_config()
 
 
 def start():
-    SingletonGroupZap().download_json_file_parser_in_memory()
     server = Server(instances=config_challenge.AMOUNT_PROCESS, port=config_challenge.PORT_SERVER)
     server.start(views)
 
